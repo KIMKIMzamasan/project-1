1069582cb9814eac22ef9d373f17d5cf7b4b6102
KIMKIMzamasan <ckimkimsama@gmail.com>
2025-01-21 11:44:22 +0800

    commited

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
     
         <p></p>
     
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, quasi expedita? Mollitia eveniet, a natus, impedit molestias et tempore, corrupti architecto deleniti officiis rem minus eaque vel distinctio dignissimos quaerat.</p>
+        <p>As a 4th year student, I love playing games like: League of Legends, Valorant, Terraria, Minecraft, Roblox, Tetris, Strinova, Risk of Rain 2, and Battlefield 4 - this is my stress reliever, after working on an assignment or after school I play video games to ease my self and to relax for a little bit.</p>
     </div>
     
     <div class="picture">
@@ -28,7 +28,7 @@
 </div>
 
 <div class="description">
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam odio tenetur reprehenderit in. Ipsam nostrum illum consectetur! Ab ratione doloremque velit quaerat ipsum amet incidunt officiis, eum quo voluptate doloribus?</p>
+    <p>There are hobbies I am really good at, and I used them to compete in school competitions and outside of school.</p>
 </div>
 
 <div class="container_2">
@@ -36,7 +36,7 @@
     
         <p></p>
     
-        <p>Hi, my name is Czar Von Louie Cabural, enthusiastic about technology and programming, proficient in designing database schema, and passionate about solving problems to deliver web-based solutions that efficiently streamline processes.</p>
+        <p>I also play chess, this is my favorite board games to play when I was a kid and up until now, this game helps me to built up a strategic mind and strong critical decision skills to later on be used as I progress in age.I have competed multiple contest about this game, gaining valuable experience that has further enhanced my skills and passion for chess.</p>
     </div>
     
     <div class="picture">
@@ -46,7 +46,7 @@
     
         <p></p>
     
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, quasi expedita? Mollitia eveniet, a natus, impedit molestias et tempore, corrupti architecto deleniti officiis rem minus eaque vel distinctio dignissimos quaerat.</p>
+        <p>I also like to sing, this is a talent I've discovered later on that I have but doesn't notice while I was younger. I love to sing everytime, it doesn't matter if it's in the bathrooom, kitchen, room, or outside. I've honed this talent and joing singing contest in school and outside school.</p>
     </div>
     
     <div class="picture">
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,8 +2,12 @@
 
 </script>
 
+<div class="picture">
+    <img src="Me.jpg" alt="Me" style="border-radius: 50%; width: 500px">
+</div>
+
 <div class="about">
-    <h2>About Us</h2>
+    <h2>About Me</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, fuga voluptatibus, nam voluptas impedit, alias ipsa quisquam temporibus repellendus quis blanditiis tempora recusandae illo quasi ex similique. Placeat, nemo itaque! Recusandae libero labore repellat ea quos at quaerat unde, itaque sequi dolorem mollitia quia molestiae assumenda odit nihil rem incidunt tenetur. Dicta saepe soluta atque quidem aperiam sint odio qui?</p>
 
 </div>
@@ -13,6 +17,24 @@
         text-align: center;
         display: block;
         margin: 20px auto;
+    }
+
+    .about h2{
+        font-size: 50px;
+    }
+
+    .about p{
+        text-align: justify;
+        font-size: 30px;
+        display: block;
+        margin: 20px;
+    }
+
+    .picture{
+        display: block;
+        margin: auto 1rem;
+        padding: 2rem 2rem;
+        text-align: center;
     }
 
 
