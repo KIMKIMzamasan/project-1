c0113a555fd0163eb95653e33e7a71b447a5fe2d
KIMKIMzamasan <ckimkimsama@gmail.com>
2025-02-01 21:52:44 +0800

    nice

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,24 +6,68 @@
     <img src="Me.jpg" alt="Me" style="border-radius: 50%; width: 500px">
 </div>
 
-<div class="about">
     <h2>About Me</h2>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, fuga voluptatibus, nam voluptas impedit, alias ipsa quisquam temporibus repellendus quis blanditiis tempora recusandae illo quasi ex similique. Placeat, nemo itaque! Recusandae libero labore repellat ea quos at quaerat unde, itaque sequi dolorem mollitia quia molestiae assumenda odit nihil rem incidunt tenetur. Dicta saepe soluta atque quidem aperiam sint odio qui?</p>
 
-</div>
+    <div class="container">
+
+        <div class="about">
+            <p>Name - Czar Von Louie Cabural</p>
+            <p>Birthdate - September 24, 2002</p>
+            <p>Address - National Highway, Isulan Sultan Kudarat</p>
+            <p></p>
+            <p style="font-weight: bold;" >Skills</p>
+            <p>Experience in using Microsoft Office tools</p>
+            <p>Proficient in verbal and written communications both Filipino and English</p>
+            <p>Experienced in structuring and designing database schema</p>
+            <p>Experienced in Web Development</p>
+
+        </div>
+
+        <div class="about2">
+            <p>Backend Framework: Svelte (Beginner)</p>
+            <p>Javascript (Beginner)</p>
+            <p>dbDiagram</p>
+            <p>C#</p>
+            <p>Python</p>
+            <p>Java</p>
+            <p>MSSQL and MySQL</p>
+            <p>HTML and CSS (Beginner)</p>
+        </div>
+
+        
+
+    </div>
+
+    
 
 <style>
     .about{
         text-align: center;
         display: block;
         margin: 20px auto;
+        
     }
 
-    .about h2{
+    h2{
         font-size: 50px;
+        text-align: center;
+        display: block;
     }
 
     .about p{
+        text-align: justify;
+        font-size: 30px;
+        display: block;
+        margin: 20px;
+    }
+
+    .about2{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+   
+    }
+    .about2 p{
         text-align: justify;
         font-size: 30px;
         display: block;
@@ -37,5 +81,10 @@
         text-align: center;
     }
 
-
+    .container{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px; 
+   
+    }
 </style>
