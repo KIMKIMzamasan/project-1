76f1ecd5970a6b1dd1b4ebd726cc651bd8df86b3
KIMKIMzamasan <ckimkimsama@gmail.com>
2025-02-04 16:27:37 +0800

    added media query for responsive design, added pictures

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="picture">
-    <img src="Me.jpg" alt="Me" style="border-radius: 50%; width: 500px;  box-shadow: 5px 5px 10px #8A2BE2 ">
+    <img src="Me2.jpeg" alt="Me">
 </div>
 
     <h2>About Me</h2>
@@ -56,6 +56,20 @@
             <img src="27.jpg" alt="">
             <img src="28.jpg" alt="">
             <img src="29.jpg" alt="">
+            <img src="55.jpg" alt="">
+            <img src="56.jpg" alt="">
+            <img src="57.jpg" alt="">
+            <img src="58.jpg" alt="">
+            <img src="59.jpg" alt="">
+            <img src="60.jpg" alt="">
+            <img src="61.jpg" alt="">
+            <img src="62.jpg" alt="">
+            <img src="63.jpg" alt="">
+            <img src="64.jpg" alt="">
+            <img src="65.jpg" alt="">
+            <img src="66.jpg" alt="">
+            <img src="67.jpg" alt="">
+            <img src="68.jpg" alt="">
     </div>
 
 
@@ -75,10 +89,37 @@
         <img src="12.jpg" alt="">
         <img src="13.jpg" alt="">
         <img src="14.jpg" alt="">
-
+        <img src="41.jpg" alt="">
+        <img src="42.jpg" alt="">
+        <img src="43.jpg" alt="">
+        <img src="44.jpg" alt="">
+    </div>
+        <h2>Other</h2>
+        
+    <div class="Other">
+            <img src="38.jpg" alt="">
+            <img src="39.jpg" alt="">
+            <img src="40.jpg" alt="">
+  
+            <img src="46.jpg" alt="">
+            <img src="47.jpg" alt="">
+            <img src="48.jpg" alt="">
+            <img src="49.jpg" alt="">
+            <img src="50.jpg" alt="">
+            <img src="52.jpg" alt="">
+            <img src="53.jpg" alt="">
+            <img src="54.jpg" alt="">
     </div>
 
 <style>
+
+    .picture img{
+        border-radius: 
+        50%; width: 
+        500px;  
+        box-shadow: 5px 5px 10px #8A2BE2 
+    }
+
     .about{
         text-align: center;
         display: block;
@@ -184,6 +225,178 @@
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%) scale(5);
+    } 
+
+    .Other{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+        grid-gap: 20px;
     }
+
+    .Other img {
+        width: 150px;
+        height: 150px;
+        object-fit: cover;
+        box-shadow: 5px 5px 10px #8A2BE2;
+        transition: transform 0.3s, box-shadow 0.3s;
+        cursor: pointer;
+    }
+
+    .Other img:hover {
+        transform: scale(1.1);
+        box-shadow: 10px 10px 20px #8A2BE2;
+    }
+
+    .Other img:active {
+        transform: scale(5);
+        box-shadow: 15px 15px 30px #8A2BE2;
+        z-index: 10;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) scale(5);
+    }
+
+    @media (max-width: 430px){
+    h2 {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+    font-size: 100px;
+    }
+
+    .container{
+    display: block; 
+    grid-template-columns: 1fr 1fr; 
+    grid-gap: 20px; 
+    width: 50% auto; 
+    }
+
+    .picture{
+    display: block;
+    margin: 1rem;
+    padding: 1rem 1rem;
+    text-align: center;
+    }
+
+    .picture img{
+        width: 200px;     
+        border-radius: 50%; 
+        width: 200px;  
+        box-shadow: 5px 5px 10px #8A2BE2
+    }
+
+    .Tagisan{
+        padding-left: 40px;
+    }
+
+    .Tagisan img:active{
+        transform: scale(2);
+        box-shadow: 15px 15px 30px #8A2BE2;
+        z-index: 10;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) scale(2);
+    }
+    .School{
+        padding-left: 40px;
+    }
+
+    .School img:active{
+        transform: scale(2);
+        box-shadow: 15px 15px 30px #8A2BE2;
+        z-index: 10;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) scale(2);
+    }
+    .Other{
+        padding-left: 40px;
+    }
+
+    .Other img:active{
+        transform: scale(2);
+        box-shadow: 15px 15px 30px #8A2BE2;
+        z-index: 10;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) scale(2);
+    }
+
+}
+
+@media (max-width: 540px){
+    h2 {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+    font-size: 100px;
+    }
+
+    .container{
+    display: block; 
+    grid-template-columns: 1fr 1fr; 
+    grid-gap: 20px; 
+    width: 50% auto; 
+    }
+
+    .picture{
+    display: block;
+    margin: 1rem;
+    padding: 1rem 1rem;
+    text-align: center;
+    }
+
+    .picture img{
+        width: 200px;     
+        border-radius: 50%; 
+        width: 200px;  
+        box-shadow: 5px 5px 10px #8A2BE2
+    }
+
+    .Tagisan{
+        padding-left: 15px;
+    }
+
+    .Tagisan img:active{
+        transform: scale(2);
+        box-shadow: 15px 15px 30px #8A2BE2;
+        z-index: 10;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) scale(2);
+    }
+    .School{
+        padding-left: 15px;
+    }
+
+    .School img:active{
+        transform: scale(2);
+        box-shadow: 15px 15px 30px #8A2BE2;
+        z-index: 10;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) scale(2);
+    }
+    .Other{
+        padding-left: 15px;
+    }
+
+    .Other img:active{
+        transform: scale(2);
+        box-shadow: 15px 15px 30px #8A2BE2;
+        z-index: 10;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) scale(2);
+    }
+
+}
 
 </style>
