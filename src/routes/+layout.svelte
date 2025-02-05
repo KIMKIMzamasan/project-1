959fa48e069fe1590eeb31e146c48cc956e6ddf3
KIMKIMzamasan <ckimkimsama@gmail.com>
2025-02-05 10:01:08 +0800

    fixed some errors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,43 @@ import '../routes/global.css';
     <slot> </slot>
 </main>
 <footer>
-    <p style="color: aliceblue;" >Copy Right 2025 Mazasankun</p>
+    <h3 style="color: aliceblue;" >Copy Right 2025 Mazasankun</h3>
+    
+    <div class="container">
+        <div class="sec1">
+
+            <div class="pictures">
+                <img src="facebook.svg" alt="">
+                <a href="https://www.facebook.com/kimkimsama.cabural.1">czar cabural</a>
+            </div>
+
+            <div class="pictures">
+                <img src="instagram.svg" alt="">
+                <a href="https://www.instagram.com/cabural_czar/">czar_cabural</a>
+            </div>
+
+            <div class="pictures">
+                <img src="github.svg" alt="">
+                <a href="https://github.com/KIMKIMzamasan">KIMKIMzamasan</a>
+            </div>
+
+        </div>
+
+        <div class="sec2">
+            <div class="pictures">
+                <img src="linkedin.svg" alt="">
+                <a href="https://www.linkedin.com/in/czar-von-louie-cabural-153a91332/">Czar Von Louie Cbaural</a>
+            </div>
+
+            <div class="pictures">
+                <img src="gmail.svg" alt="">
+                <a href="https://www.gmail.com/">caburalczarvonlouie@gmail.com</a>
+            </div>
+        </div>
+    </div>
+
+    
+
 </footer>
 
 <style>
@@ -35,11 +71,37 @@ import '../routes/global.css';
         margin: 20px auto;
     }
     footer{
-        display: flex;
-        justify-content:center;
+        display: block;
         background-color: #8A2BE2;
+        height: 300px;
+        padding: 20px;
+    }
+
+    footer h3{
+        text-align: center;
+        font-size: 20px;
+    }
+
+    .container{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        justify-content: justify;
+    }
+
+
+    .pictures img{
+        width: 30px;
+        margin-left: 200px; 
 
     }
+
+    .pictures a{
+        text-align:justify;
+        color: aliceblue;
+        font-size: 20px;
+        text-decoration: none;
+    }
+
     .about{
     margin-top: 20px;
     margin: 10px;
